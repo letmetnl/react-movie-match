8ee194899e99af1956e14bc054dd6bfc35c1c631
mohits Projects <mohit5150515@gmail.com>
2020-08-14 16:25:56 +0530

    added action n reducers

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 class MovieCard extends React.Component {
     render(){
+        const { movie } = this.props;
         return(
             <div className="movie-card">
                 <div className="left">
-                    <img alt="movie-poster" src={MovieCard.Poster} />
+                    <img alt="movie-poster" src={movie.Poster} />
                 </div>
                 <div className="right">
-                    <input />
-                    <button id="search-btn">Search</button>
+                    <div className="title">
+                        {movie.Title}
+                    </div>
+                    <div className="title">
+                        {movie.Plot}
+                    </div>
+                    <div className="footer">
+                        <div className="rating">
+                            {movie.imdbRating}
+                        </div>
+                        <button className="favourite-btn">Favourite</button>
+                    </div>
                 </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// importing packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+// import { Provider } from 'react-redux';
+
+// importing files
 import './index.css';
 import App from './component/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
