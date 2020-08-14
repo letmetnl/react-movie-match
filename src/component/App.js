1f06a03ec2df90cfb94c8eca2ea1fc8712f1b6df
mohits Projects <mohit5150515@gmail.com>
2020-08-14 20:03:45 +0530

    changing the state

--- src/component/App.js
+++ src/component/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
     console.log('state', this.props.store.getState());
   }
   render(){
-    const movies = this.props.store.getState();
-    console.log('rendered');
+    const {list} = this.props.store.getState(); //{list:[], favourites:[]}
+    console.log('rendered', this.props.store.getState());
     return (
       <div className="App">
           <Navbar />
@@ -29,7 +29,7 @@ class App extends React.Component {
               </div>
           
             <div className="list">
-                {movies.map((movie, index) => (
+                {list.map((movie, index) => (
                   <MovieCard movie={movie} key={`movies-${index}`} />
                 ))}
             </div>
