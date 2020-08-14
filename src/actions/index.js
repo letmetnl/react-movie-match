4b0e3aa96b2c2c76649a1a6e9d0fceec71dc129f
mohits Projects <mohit5150515@gmail.com>
2020-08-14 19:45:53 +0530

    action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 // {
 //     type: 'ADD_MOVIES'
 // }
+
+// action types variables
+export const ADD_MOVIES = 'ADD_MOVIES';
+// action creators for returning actions
+export function addMovies(movies){
+    return {
+            type: ADD_MOVIES,
+            movies
+        }
+}
